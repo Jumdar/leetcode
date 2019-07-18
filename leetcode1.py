67ba6625632ecf2365418195d7dc887a2f8b59ce
hanzongda <hanzongda123@163.com>
2019-07-18 17:14:48 +0800

    两数之和

---
+++ leetcode1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums, target):
+        result = []
+        for i in range(len(nums)):
+            temp = target - nums[i]
+            for j in range(i + 1, len(nums)):
+                if nums[j] == temp:
+                    result.append(i)
+                    result.append(j)
+
+        return result
