fc7d106010c57e9cb96ddb4b99ca78fa5a0c3fdd
hanzongda <hanzongda123@163.com>
2019-09-12 14:21:01 +0800

    9.12

---
+++ swapPairs.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prehead = ListNode(-1)
+        prehead.next = head
+        c = prehead
+        while c.next and c.next.next:
+            a, b = c.next, c.next.next
+            c.next, a.next = b, b.next
+            b.next = a
+            c = c.next.next
+
+        return prehead.next
